de912026793fac42c549b7a28296affa25f5691b
Carolina943 <akamifox943@gmail.com>
2022-08-11 20:28:23 -0400

    file modified

--- http-request-Object.js
+++ http-request-Object.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const {readFileSync} = require('fs');
 
+
 //get all files
-const homepage = require('./index.html')
+const homePage = readFileSync('./index.html');
+
 
  const server = http.createServer(function(req, res){
     // console.log(req.method)
   const url = req.url;
   if(url === '/'){
     res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<h1>home page</h1>')
+    res.write(homePage)
     res.end();
   }
   //about page
