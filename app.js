6d6afbcd166d8009b7f8e32c5e31f609bf1525d9
Carolina943 <akamifox943@gmail.com>
2022-08-17 10:08:18 -0400

    modified file

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// setup static and middleware
-app.use(express.static('./public'))
-
+const express = require('express'); 
+const app = express(); 
+const {products} = require('./data')
 
 app.get('/', function(req,res){
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.json(products)
 })
 
-
-app.all('*', function(req,res){
-    res.status(404).send('resource not found')
-})
 
 app.listen(5000, function(){
-    console.log('server is listening on port 5000...')
+    console.log('Server is listening on port 5000...')
 })
