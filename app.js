90c31181342543e3d093b0fcc756dd388094e93c
Carolina943 <akamifox943@gmail.com>
2022-08-17 16:59:10 -0400

    modified file

--- app.js
+++ app.js
@@ -46,6 +46,28 @@ app.get('/api/products/:productID/reviews/:reviewID', function(req,res){
 })
 
 
+app.get('/api/v1/query', function(req,res){
+    //console.log(req.query)
+    const { search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product)=>{
+        return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1){
+      //res.status(200).send('no products matched your search)
+      return res.status(200).json({success:true, date: []})
+    }
+    res.status(200).json(sortedProducts)
+    res.send("Hello World")
+})
+
+
 app.listen(5000, function(){
     console.log('Server is listening on port 5000...')
 })
