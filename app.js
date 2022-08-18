805045fcab4bc75113521166aa0ab7c87a4a7c80
Carolina943 <akamifox943@gmail.com>
2022-08-18 11:38:38 -0400

    modified file

--- app.js
+++ app.js
@@ -1,72 +1,25 @@
 const express = require('express'); 
 const app = express(); 
-const {products} = require('./data')
 
+//req => middleware => res
 
-app.get('/', function(req,res){
-    res.send('<h1>Home Page</h1><a href= "/api/products">products</a>')
-})
-
-
-app.get('/api/products', function(req,res){
- const newProducts = products.map((products)=>{
-    const {id, name, image} = products;
-    return {id, name, image}
- })
-    res.json(newProducts)
-})
-/*
-app.get('/api/products/1', function(req,res){
-    const singleProduct = products.find((product) => product.id === 1)
-    res.json(singleProduct)
-})
-*/
-
-
-app.get('/api/products/:productID', function(req, res){
- /*
- console.log(req)
- console.log(req.params)
- */
-const {productID} = req.params;
-
- const singleProduct = products.find((product) => product.id === Number(productID)
- 
-)
-if(!singleProduct){
-    return res.status(404).send('Product Does Not Exist')
+const logger = (req, res, next) =>{
+    const method = req.method;
+    const url = req.url
+    const time = new Date();
+    console.log(method, url, time)
+    //res.send('Testing')
+    next();
 }
- return res.json(singleProduct)
+
+app.get('/', logger, function(req,res){
+   
+    res.send('Home')
 })
 
-
-app.get('/api/products/:productID/reviews/:reviewID', function(req,res){
-    console.log(req.params);
-    res.send("Hello World");
+app.get('/about', function(req, res){
+    res.send('About')
 })
-
-
-app.get('/api/v1/query', function(req,res){
-    //console.log(req.query)
-    const { search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if(search){
-        sortedProducts = sortedProducts.filter((product)=>{
-        return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if(sortedProducts.length < 1){
-      //res.status(200).send('no products matched your search)
-      return res.status(200).json({success:true, date: []})
-    }
-    res.status(200).json(sortedProducts)
-    res.send("Hello World")
-})
-
 
 app.listen(5000, function(){
     console.log('Server is listening on port 5000...')
